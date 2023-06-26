61df5295ce27d594d1d45108cc668dd993400ed3
phnaharris <phnanh.harris@gmail.com>
2023-06-26 20:31:45 +0700

    feat(env): fastify/env work like the config manager, feel better than my way

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
+import { fastifyEnv } from "@fastify/env";
 import AutoLoad from "@fastify/autoload";
 
-export const options = {};
+const schema = {
+  type: "object",
+  required: ["PORT"],
+  properties: {
+    PORT: {
+      type: "string",
+      default: 3000,
+    },
+  },
+};
+
+export const options = {
+  schema: schema,
+};
 
 export default async function (fastify, opts) {
   const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +24,11 @@ export default async function (fastify, opts) {
   // Place here your custom code!
 
   // Do not touch the following lines
+  fastify.register(fastifyEnv, options).ready((err) => {
+    if (err) console.error(err);
+
+    console.log(fastify.config); // or fastify[options.confKey]
+  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
